a5755d45e21e9a0da7bcd4ba9f6f5c58ed63eb22
Stanislav Yakushev <stas.ghost@gmail.com>
2017-10-25 20:13:30 +0300

    Json field is composed via string interpolation instead of json_create_object Reason json_create_object too slow when called many times eith a lot of args.

--- util/functions.sql
+++ util/functions.sql
@@ -211,7 +211,7 @@ CREATE OR REPLACE FUNCTION insert_observations(_total_count_ integer)
 RETURNS bigint AS $$
 with observations_source as (
   select id as loinc_code,
-         short_name as loinc_name,
+         long_name as loinc_name,
          row_number() over ()
     from common_observations
     cross join generate_series(0, ceil(_total_count_::float
@@ -238,69 +238,82 @@ with observations_source as (
     SELECT (obj->>'id'), 0, 'created', obj
     FROM (
       SELECT
-        json_build_object(
-         'resourceType', 'Observation',
-         'id', nextval('observation_id')::text,
-         'meta', json_build_object(
-            'versionId', gen_random_uuid(),
-            'lastUpdated', CURRENT_TIMESTAMP
-          ),
-         'status', 'final',
-         'identifier', ARRAY[
-           json_build_object(
-             'use', 'official',
-             'system', 'http://www.bmc.nl/zorgportal/identifiers/observations',
-             'value', random(6320, 6329)::text
-           )],
-         'code', json_build_object('coding', ARRAY[
-           json_build_object(
-           'system', 'loinc.org',
-           'code', loinc_code,
-           'display', loinc_name)]),
-         'subject', json_build_object(
-             'id', patient_id,
-             'resourceType', 'Patient',
-             'display', patient_name || ' '::text || patient_family),
-         'effective', json_build_object(
-                          'period', json_build_object(
-                                    'start', random_datetime(2012, 2013),
-                                    'end', random_datetime(2014, 2015))),
-         'issued', random_datetime(2012, 2012),
-         'performer', ARRAY[
-           json_build_object(
-             'resourceType', 'Practitioner',
-             'id', ceil(random() * 1000),
-             'display', 'Random Name'
-          )],
-         'value', json_build_object(
-           'quantity', json_build_object(
-             'value', random(1,10),
-             'unit', 'g/dl',
-             'code', 'g/dl',
-             'system', 'http://unitsofmeasure.org'
-          )),
-          'interpretation', json_build_object(
-                              'coding', ARRAY[
-                                'system', 'http://hl7.org/fhir/v2/0078',
-                                'code', 'L',
-                                'display', 'Low'
-                             ]),
-          'referenceRange', ARRAY[
-            json_build_object(
-              'low', json_build_object(
-                'value', random(10,14),
-                'unit', 'g/dl',
-                'code', 'g/dl',
-                'system', 'http://unitsofmeasure.org'
-            )),
-            json_build_object(
-              'high', json_build_object(
-              'value', random(15,20),
-              'unit', 'g/dl',
-              'code', 'g/dl',
-              'system', 'http://unitsofmeasure.org'
-           ))]
-        )::jsonb as obj
+      format('{"resourceType": "Observation",
+         "id" : %s,
+         "meta": {
+            "versionId": %I,
+            "lastUpdated": %I
+          },
+         "status": "final",
+         "identifier": [
+           {
+             "use": "official",
+             "system": "http://www.bmc.nl/zorgportal/identifiers/observations",
+             "value": %I
+           }],
+         "code": {"coding": [
+           {
+             "system": "loinc.org",
+             "code": %I,
+             "display": %I
+           }]},
+         "subject": {
+             "id": %s,
+             "resourceType": "Patient",
+             "display": %I
+           },
+         "effective": {
+             "period": {
+                 "start": %I,
+                 "end": %I}},
+         "issued": %I,
+         "performer": [
+           {
+             "resourceType": "Practitioner",
+             "id": %I,
+             "display": "Random Name"
+          }],
+         "value": {
+           "quantity": {
+             "value": 3,
+             "unit": "g/dl",
+             "code": "g/dl",
+             "system": "http://unitsofmeasure.org"
+          }},
+          "interpretation": {
+                              "coding": {
+                                "system": "http://hl7.org/fhir/v2/0078",
+                                "code": "L",
+                                "display": "Low"
+                             }},
+          "referenceRange": [
+            {
+              "low": {
+                "value": 234,
+                "unit": "g/dl",
+                "code": "g/dl",
+                "system": "http://unitsofmeasure.org"
+            }},
+            {
+              "high": {
+              "value": 17,
+              "unit": "g/dl",
+              "code": "g/dl",
+              "system": "http://unitsofmeasure.org"
+           }}]
+           }', nextval('observation_id'),
+               gen_random_uuid(),
+               CURRENT_TIMESTAMP,
+               random(6320, 6329),
+               loinc_code,
+               loinc_name,
+               patient_id,
+               patient_name || ' '::text || patient_family,
+               random_datetime(2012, 2013),
+               random_datetime(2014, 2015),
+               random_datetime(2012, 2012),
+               ceil(random() * 1000)
+           )::json as obj
         FROM observation_data
         LIMIT _total_count_
     ) _
